24f227cbea1ee1df5533dac37fcd61f7ac6777b0
Nicholas Glazer <glazer.nicholas@gmail.com>
2024-09-17 22:08:51 +0000

    initial

---
+++ miozu-theme.js
@@ -0,0 +1,25 @@
+//
+// miozu theme,
+// miozu.com for docs
+//
+
+const miozu = {
+ base0: '#232733', // [0..7] are shades of black to white
+ base1: '#2C3040',
+ base2: '#3E4359',
+ base3: '#565E78',
+ base4: '#737E99',
+ base5: '#D0D2DB',
+ base6: '#F3F4F7',
+ base7: '#FAFBFD',
+ base8: '#EB3137', // red
+ base9: '#FF9837', // orange
+ base10: '#6DD672', // green
+ base11: '#E8D176', // yellow
+ base12: '#83D2FC', // blue
+ base13: '#C974E6', // magenta
+ base14: '#FF9982', // peach
+ base15: '#40FFE2' // cyan
+};
+
+export default miozu;
